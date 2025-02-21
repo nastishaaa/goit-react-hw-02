734c1fbb3ba5dfed39875d1d7aae187dcb6a944c
Anastasiia <anastasiiprfnk@gmail.com>
2025-02-21 16:43:19 +0200

    hw styles

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,9 +1,11 @@
+import css from './Options.module.css'
+
 export default function Options({updateFeedback, resetFeedback, totalFeedback}){
     const isFeedback = totalFeedback > 0;
-    return (<div>
-        <button onClick={() => updateFeedback("good")}>Good</button>
-        <button onClick={() => updateFeedback("bad")}>Bad</button>
-        <button onClick={() => updateFeedback("neutral")}>Neutral</button>
-        {isFeedback && <button onClick={resetFeedback}>Reset</button>}
+    return (<div className={css.btnContainer}> 
+        <button className={css.btnItem} onClick={() => updateFeedback("good")}>Good</button>
+        <button className={css.btnItem} onClick={() => updateFeedback("bad")}>Bad</button>
+        <button className={css.btnItem} onClick={() => updateFeedback("neutral")}>Neutral</button>
+        {isFeedback && <button className={css.btnItem} onClick={resetFeedback}>Reset</button>}
     </div>)
 }
